498cce64173bc46820664dc2a92a12be266645f7
Jazer Salazar <48846249+jazersalazar@users.noreply.github.com>
2022-04-13 09:40:06 +0800

    feat(core): add status system

---
+++ Assets/Scripts/Status.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status : MonoBehaviour
+{
+    private int hp;
+    private int stamina;
+
+    public int HP
+    {
+        get { return hp; }
+        set { hp = value; }
+    }
+
+    public int Stamina
+    {
+        get { return stamina; }
+        set { stamina = value; }
+    }
+}
