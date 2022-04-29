546f36b500e5a83abe89ffab4a3cc22bebd04229
Jazer Salazar <48846249+jazersalazar@users.noreply.github.com>
2022-04-29 17:32:08 +0800

    feat(menu): add menu scene

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
+
+public class MenuUIHandler : MonoBehaviour 
+{
+    public void StartNew()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void ExitToMainMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void Exit()
+    {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
+    }
+}
